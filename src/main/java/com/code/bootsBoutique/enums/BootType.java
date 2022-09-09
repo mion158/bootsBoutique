96386fe54270d6eabb1755fa55557f174744c1be
mion158 <111487792+mion158@users.noreply.github.com>
2022-09-09 14:43:14 +0100

    skeleton

---
+++ src/main/java/com/code/bootsBoutique/enums/BootType.java
@@ -0,0 +1,19 @@
+package com.code.bootsBoutique.enums;
+
+public enum BootType {
+    CHELSEA,
+    CHUKKA,
+    DRESS,
+    COWBOY,
+    GOGO,
+    RUSSIAN,
+    UGG,
+    RIGGER,
+    RAIN,
+    RACING,
+    MOTOCROSS,
+    WADER,
+    GALOSH,
+    SNOW,
+    TOURING
+   }
